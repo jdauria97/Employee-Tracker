db17fd302a7d659eb3eaf8907a1fe87b1ba2b0a0
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2024-01-04 18:46:59 -0500

    adds view functions

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const inquirer = require("inquirer")
 const mysql = require("mysql")
+
+function startPrompt() {
+
+}
+
+function viewEmployees() {
+
+}
+
+function viewRoles() {
+
+}
+
+function viewDepartments() {
+
+}
+
