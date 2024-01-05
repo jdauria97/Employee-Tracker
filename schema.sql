e584c31538884f16ff5cd1c47ef8bf7ce11d420c
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2024-01-04 20:06:45 -0500

    adds tables to schema

--- schema.sql
+++ schema.sql
@@ -4,3 +4,26 @@ CREATE DATABASE tracker_db;
 
 USE tracker_db;
 
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
